3bda3fadc4e135643634c685e59bfa11c164c7aa
jasmintrotter <jasmintmail@gmail.com>
2026-09-02 11:32:04 -0700

    phone number update

--- app/lib/site.ts
+++ app/lib/site.ts
@@ -1,7 +1,7 @@
 export const site = {
   name: "Bright & Tidy Cleaning",
-  phoneDisplay: "(503) 912-2067",
-  phoneHref: "tel:+15039122067",
+  phoneDisplay: "971-808-8538",
+  phoneHref: "tel:+19718088538",
   quoteEmail: "bright.and.tidy.cleaning.pdx@gmail.com",
   tagline:
     "Reliable, detail-oriented house cleaning in Portland, Oregon and surrounding areas.",
