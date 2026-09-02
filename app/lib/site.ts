0c73fe5a35d142f49cc27b9c28b7fdc527b650f6
jasmintrotter <jasmintmail@gmail.com>
2026-09-02 11:43:24 -0700

    phone number formatting

--- app/lib/site.ts
+++ app/lib/site.ts
@@ -1,6 +1,6 @@
 export const site = {
   name: "Bright & Tidy Cleaning",
-  phoneDisplay: "971-808-8538",
+  phoneDisplay: "(971) 808-8538",
   phoneHref: "tel:+19718088538",
   quoteEmail: "bright.and.tidy.cleaning.pdx@gmail.com",
   tagline:
